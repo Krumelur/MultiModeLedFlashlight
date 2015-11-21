7b4dc1c46a9b61186c38e9434851038784c84785
René Ruppert <rene.ruppert@gmail.com>
2015-11-21 22:40:07 +0100

    Kind of complete

---
+++ MultiModeLamp/InfoController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MultiModeLamp
+{
+	partial class InfoController : UIViewController
+	{
+		public InfoController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
