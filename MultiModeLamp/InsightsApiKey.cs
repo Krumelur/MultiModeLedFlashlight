ed94480694996f80fa400ac80b174267863c2652
René Ruppert <rene.ruppert@gmail.com>
2015-11-23 19:25:06 +0100

    Remove Insights Key

--- MultiModeLamp/InsightsApiKey.cs
+++ MultiModeLamp/InsightsApiKey.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Xamarin;
 
 namespace MultiModeLamp
 {
 	public static class InsightsApiKey
 	{
-		public const string KEY = "2dd89a9da6222a1d668014a8f5285a5e2adc36ee";
+		// TODO: Use your own Insights Key!
+		public const string KEY = Insights.DebugModeKey;
 	}
 }
 
